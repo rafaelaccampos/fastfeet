9d6cafbd1802d364f867a9dacdaa1def6cbaca2b
rafaelaccampos <rafaelacassianocampos1996@gmail.com>
2020-02-19 01:45:17 -0300

    feat: Created user registration

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rafaela Campos',
-    email: 'rafac@rocket.com.br',
-    password_hash: '1234567819',
-  });
-  return res.json({ user });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
