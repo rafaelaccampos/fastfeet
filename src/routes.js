de5f7154ea4259b88f9ac3071c620120dedb4785
rafaelaccampos <rafaelacassianocampos1996@gmail.com>
2020-02-19 00:57:49 -0300

    Created models loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rafaela Campos',
+    email: 'rafac@rocket.com.br',
+    password_hash: '1234567819',
+  });
+  return res.json({ user });
+});
 
 export default routes;
