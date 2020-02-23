49cb5f33085a2c740dbd2734acf4d5816fbb3dc2
rafaelaccampos <rafaelacassianocampos1996@gmail.com>
2020-02-23 20:39:47 -0300

    feat: Created courier model

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import Courier from '../app/models/Courier';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, Recipient, Courier];
 
 class Database {
   constructor() {
