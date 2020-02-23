60e52e2a5ffa3bcc47a04e5e811561f0c9406dce
rafaelaccampos <rafaelacassianocampos1996@gmail.com>
2020-02-23 01:07:14 -0300

    feat: Created model Recipient

---
+++ src/app/models/Recipient.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Recipient extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        street: Sequelize.STRING,
+        number: Sequelize.NUMBER,
+        complement: Sequelize.STRING,
+        city: Sequelize.STRING,
+        cep: Sequelize.NUMBER,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
+
+export default Recipient;
