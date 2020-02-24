de6b5edc580b3f1baedbb2759adf455b4277c4ea
rafaelaccampos <rafaelacassianocampos1996@gmail.com>
2020-02-23 21:54:52 -0300

    feat: Configuring multer to upload courier's photos

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+/* import CourierController from './app/controllers/CourierController'; */
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -17,5 +21,15 @@ routes.put('/users', UserController.update);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
+
+/* routes.get('/couriers', CourierController.index);
+routes.get('/couriers/:id', CourierController.show);
+routes.post('/couriers', CourierController.store);
+routes.put('/couriers/:id', CourierController.update);
+routes.delete('/couriers/:id', CourierController.delete); */
 
 export default routes;
