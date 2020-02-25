f08758e577d5183b61931e7f8d60514b0a86643f
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-02-25 14:27:32 -0300

    feat: Add model and table delivery and model and table signature

---
+++ src/app/models/Delivery.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Delivery extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        recipient_id: Sequelize.INTEGER,
+        courier_id: Sequelize.INTEGER,
+        signature_id: Sequelize.INTEGER,
+        product: Sequelize.STRING,
+        canceled_at: Sequelize.DATE,
+        start_date: Sequelize.DATE,
+        end_date: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'signature_id' });
+  }
+}
+
+export default Delivery;
